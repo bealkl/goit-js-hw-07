f3d81b9c8af1990edbca5518b2993118dcd5dee2
Oleksandr Bevzytskyi <bealkl@gmail.com>
2020-06-07 12:20:07 +0300

    make task 8

--- task-8/script-hw7-8.js
+++ task-8/script-hw7-8.js
@@ -1,12 +1,45 @@
-//GOIT javascript homework 07 task 2
+//GOIT javascript homework 07 task 8
 
-const ingredients = ['Картошка', 'Грибы', 'Чеснок', 'Помидоры', 'Зелень', 'Приправы', ];
+const elementControls = document.querySelectorAll('button');
 
-const vegetablesList = document.querySelector('ul#ingredients');
+elementControls.forEach(buttonElement => buttonElement.addEventListener('click', makeSomeChanges));
 
-const ingredientsList = ingredients.map(vegetable => {
-  const newLiElement = document.createElement('li');
-  newLiElement.textContent = vegetable;
-  return newLiElement;
-});
-vegetablesList.append(...ingredientsList);
+function makeSomeChanges(e) {
+  const valueUserWish = document.querySelector('input').value;
+  switch (e.target.dataset.action) {
+    case 'render':
+      createBoxes(valueUserWish);
+      break;
+    case 'destroy':
+      destroyBoxes();
+      break;
+  }
+  document.querySelector('input').value = '';
+}
+
+function createBoxes(amount) {
+  const myDivElement = document.querySelector('div#boxes');
+  const divArray = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i += 1) {
+    const newDivElement = document.createElement('div');
+    newDivElement.style.height = newDivElement.style.width = 30 + i * 10 + 'px';
+    newDivElement.style.backgroundColor = randomColorRGB();
+    newDivElement.classList.add('divchild');
+    divArray.append(newDivElement);
+  }
+  myDivElement.append(divArray);
+}
+
+function destroyBoxes() {
+  const myDivElement = document.querySelectorAll('div.divchild');
+  for (let keyNode of myDivElement) keyNode.remove();
+  // for (let i = 0; i < myDivElement.length; i += 1) myDivElement[i].remove();
+}
+
+function randomColorRGB() {
+  const colorR = Math.floor(Math.random() * Math.floor(255));
+  const colorG = Math.floor(Math.random() * Math.floor(255));
+  const colorB = Math.floor(Math.random() * Math.floor(255));
+  return `rgb(${colorR},${colorG},${colorB})`;
+}
